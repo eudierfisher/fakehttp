c7766dd39704b2929eff93ac34180a59295c5c7c
eudierfisher <eudierfisher@gmail.com>
2024-12-23 21:01:33 +0800

    feat: 实现 SetReadDeadline 以支持 websocket
    - 在 FakeConn 上实现 SetReadDeadline、SetWriteDeadline 和 SetDeadline 方法
    - 在 FakeChannel 上添加 readDeadline 和 writeDeadline 相关逻辑
    - 优化 Read 方法以支持 deadline 检查和处理
    - 添加日志输出以方便调试

--- fake_conn.go
+++ fake_conn.go
@@ -2,7 +2,9 @@ package fakehttp
 
 import (
 	"io"
+	"log"
 	"net"
+	"os"
 	"sync"
 	"time"
 )
@@ -33,32 +35,123 @@ func (c *FakeConn) Read(b []byte) (int, error) { return c.ReadChannel.Read(b) }
 
 func (c *FakeConn) Write(b []byte) (int, error) { return c.WriteChannel.Write(b) }
 
-func (c *FakeConn) SetReadDeadline(t time.Time) error  { return nil }
-func (c *FakeConn) SetWriteDeadline(t time.Time) error { return nil }
-func (c *FakeConn) SetDeadline(t time.Time) error      { return nil }
+func (c *FakeConn) SetReadDeadline(t time.Time) error {
+	if t.IsZero() {
+		log.Println("clear read deadline")
+	} else {
+		log.Println("set read deadline", t.Format(time.Stamp))
+	}
+	return c.ReadChannel.SetReadDeadline(t)
+}
+func (c *FakeConn) SetWriteDeadline(t time.Time) error { return c.WriteChannel.SetWriteDeadline(t) }
+func (c *FakeConn) SetDeadline(t time.Time) error {
+	c.SetReadDeadline(t)
+	c.SetWriteDeadline(t)
+	return nil
+}
 
 func NewFakeChannel(maxDataCount int) *FakeChannel {
 	return &FakeChannel{
-		closeC: make(chan struct{}),
-		dataC:  make(chan []byte, maxDataCount),
+		readDeadlineC: make(chan struct{}),
+		closeC:        make(chan struct{}),
+		dataC:         make(chan []byte, maxDataCount),
 	}
 }
 
 type FakeChannel struct {
+	mu            sync.Mutex
+	readDeadline  time.Time
+	writeDeadline time.Time
+	readDeadlineC chan struct{}
+
 	closeOnce sync.Once
 	closeC    chan struct{}
 	dataC     chan []byte
 }
 
-func (c *FakeChannel) Read(b []byte) (n int, err error) {
+func (c *FakeChannel) SetReadDeadline(t time.Time) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if t == c.readDeadline {
+		return nil
+	}
+	c.readDeadline = t
 	select {
-	case <-c.closeC:
-	case data, ok := <-c.dataC:
-		if ok && len(data) <= len(b) {
-			return copy(b, data), nil
+	case c.readDeadlineC <- struct{}{}:
+		// notify Read
+	default:
+	}
+	return nil
+}
+
+func (c *FakeChannel) SetWriteDeadline(t time.Time) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.writeDeadline = t
+	return nil
+}
+
+var noDeadlineC = make(chan time.Time, 1)
+
+func (c *FakeChannel) Read(b []byte) (n int, err error) {
+	c.mu.Lock()
+	readDeadline := c.readDeadline
+	c.mu.Unlock()
+
+	if !readDeadline.IsZero() && readDeadline.Before(time.Now()) {
+		// fast check and fail
+		return 0, os.ErrDeadlineExceeded
+	}
+
+	// the check above makes sure that when the loop starts,
+	// the read deadline is always valid (0 or in the future);
+	// when the following loop blocks, it must indicate that channel dataC has been emptied
+	var ch <-chan time.Time = noDeadlineC
+	var timer *time.Timer
+	for loop := true; loop; {
+		select {
+		case dl := <-ch:
+			// entering this case means that the timer is not nil and has expired
+			log.Println("timer expired at", dl.Local().String())
+			timer = nil
+			n, err = 0, os.ErrDeadlineExceeded
+			loop = false
+			continue
+		case <-c.readDeadlineC:
+			// someone has called SetReadDeadline
+			if timer != nil {
+				timer.Stop()
+			}
+
+			c.mu.Lock()
+			readDeadline = c.readDeadline
+			c.mu.Unlock()
+			if readDeadline.IsZero() {
+				ch = noDeadlineC
+				continue
+			}
+			if readDeadline.Before(time.Now()) {
+				// deadline exceeded, cancel Read
+				n, err = 0, os.ErrDeadlineExceeded
+				loop = false
+				continue
+			}
+			timer = time.NewTimer(time.Until(readDeadline))
+			ch = timer.C
+		case <-c.closeC:
+			n, err = 0, io.EOF
+			loop = false
+		case data, ok := <-c.dataC:
+			if ok && len(data) <= len(b) {
+				n, err = copy(b, data), nil
+			}
+			loop = false
 		}
 	}
-	return 0, io.EOF
+	if timer != nil {
+		timer.Stop()
+	}
+	return
 }
 
 func (c *FakeChannel) Write(b []byte) (int, error) {
